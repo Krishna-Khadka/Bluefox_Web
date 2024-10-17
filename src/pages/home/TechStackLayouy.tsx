1c0ed2d608e9d96399e1df96148e865bc00e8667
Krishna-Khadka <krishkhadka2059@gmail.com>
2024-10-16 20:12:08 -0700

    new commit

--- src/pages/home/TechStackLayouy.tsx
+++ src/pages/home/TechStackLayouy.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
 
-import tech1 from "../../../public/icons/React.png"
-import tech2 from "../../../public/icons/Next.js.png"
-import tech3 from "../../../public/icons/Laravel.png"
-import tech4 from "../../../public/icons/Node.js.png"
-import tech5 from "../../../public/icons/Flutter.png"
-import tech6 from "../../../public/icons/MongoDB.png"
-import tech7 from "../../../public/icons/MySQL.png"
-import tech8 from "../../../public/icons/Figma.png"
-import tech9 from "../../../public/icons/AfterEffect.png"
-import tech10 from "../../../public/icons/PostgresSQL.png"
 import Image from 'next/image'
-import Link from 'next/link'
+
 import { StaticImport } from 'next/dist/shared/lib/get-img-props'
 
 interface TechStack {
